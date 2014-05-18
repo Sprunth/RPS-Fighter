0e8eac5bc6f8477398d09d138759207577e9a6b7
Shyam Pinnipati <shamanshaman654@gmail.com>
2014-05-18 10:41:38 -0700

    Writing out instruction set and window

--- RPS_Fighter/Program.cs
+++ RPS_Fighter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using SFML.Graphics;
+using SFML.Window;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,15 @@ namespace RPS_Fighter
 
             DisplayTitleScreen();
             titleWindow.Dispose();
+            DisplayInstrScreen();
+            instrScreen.Dispose();
 
             ActiveGame = new RPSGame();
             ActiveGame.Initialize();
             ActiveGame.Run();
         }
 
-        static RenderWindow titleWindow;
+        static RenderWindow titleWindow, instrScreen;
 
         private static void DisplayTitleScreen()
         {
@@ -42,9 +45,31 @@ namespace RPS_Fighter
             }
         }
 
+        private static void DisplayInstrScreen()
+        {
+            instrScreen = new RenderWindow(new SFML.Window.VideoMode(1200, 600), "RPS Fighter", SFML.Window.Styles.Close);
+            instrScreen.MouseButtonReleased += window_MouseButtonReleasedi;
+            instrScreen.SetMouseCursorVisible(false);
+            Font fonts = new Font("Fonts/cambria.ttc");
+            Text instr = new Text("Instructions:\n\nAt the beginning of each round, each player chooses a card to fight with. These cards are pitted against each other. The rules for\nvictory are simple: an Attack beats a Grapple, a Grapple beats a Block, and a Block beats an Attack. The rules become a little bit\nmore complex now.\n1. If you Block an Attack, you gain a small amount of health equal to the strength of your shield.\n2. If you Block another shield, you get a lessened amount of health.\n3. Grappling another grapples results in a test for control. The one with the higher Grapple strength will be victorious. If the two\ncards have equal strength, no damage is taken by either side.\n4. Similarly, Attacking an Attack also results in a struggle. The stronger Attack prevails. Equal-strength attacks result\nin both players losing health.\n5. A successful Grapple allows an extra action. A successful Attack allows for you to use as many attacks as your energy value\nallows, or until you choose a Block or Grapple card or run out of cards in your hand. Energy is replenished at the end of each round.\n", fonts, 22);
+            instr.Position = new Vector2f(0, 0);
+            while (instrScreen.IsOpen())
+            {
+                instrScreen.DispatchEvents();
+                instrScreen.Clear();
+                instrScreen.Draw(instr);
+                instrScreen.Display();
+            }
+        }
+
         static void window_MouseButtonReleased(object sender, SFML.Window.MouseButtonEventArgs e)
         {
             titleWindow.Close();
+        }
+
+        static void window_MouseButtonReleasedi(object sender, SFML.Window.MouseButtonEventArgs e)
+        {
+            instrScreen.Close();
         }
     }
 }
