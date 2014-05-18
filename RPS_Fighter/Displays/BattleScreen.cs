65fc08ad3d7fcd878cf7291f9504d726075b3a66
sprunth <sprunth@gmail.com>
2014-05-17 23:18:52 -0700

    Framework for battlescreen

---
+++ RPS_Fighter/Displays/BattleScreen.cs
@@ -0,0 +1,19 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS_Fighter.Displays
+{
+    class BattleScreen
+    {
+        Sprite slash, versus;
+
+        public BattleScreen()
+        {
+
+        }
+    }
+}
