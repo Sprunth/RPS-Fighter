b8a82e8ed2b89a237198d3d4ebc1f6fa088f26c8
Shyam Pinnipati <shamanshaman654@gmail.com>
2014-05-17 19:09:12 -0700

    stuff

--- RPS_Fighter/RPSGame.cs
+++ RPS_Fighter/RPSGame.cs
@@ -20,7 +20,7 @@ namespace RPS_Fighter
 
         public RPSGame()
         {
-            WindowSize = new Vector2u(1200, 900);
+            WindowSize = new Vector2u(1200, 600);
             ContextSettings cs = new ContextSettings();
             cs.AntialiasingLevel = 8;
             window = new RenderWindow(new VideoMode(WindowSize.X, WindowSize.Y),"RPS Fighter", Styles.Titlebar | Styles.Close, cs);
